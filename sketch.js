8c1a5bd7a0e6769fa8a02c9e29090e0c35e76443
king-khi <mastergameboy3@gmail.com>
2020-10-31 12:34:34 -0400

    platfrom added

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    platform = new Ground(150,305,300,170)
 }
 
 function draw(){
@@ -49,16 +49,13 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
     box3.display();
     box4.display();
     pig3.display();
     log3.display();
-
     box5.display();
     log4.display();
     log5.display();
-
     bird.display();
     platform.display();
 }
